1331444af9b8831f6dc6107d1fd949415d1b1bfd
eihwood <ellieheywood@gmail.com>
2023-10-20 13:29:14 -0400

    implemented 3fold cross validation and plots results to mirror phrophet model

--- src/sarimax_deploy.py
+++ src/sarimax_deploy.py
@@ -45,39 +45,37 @@ def mod_eval(sxmodel, endog_test, exog_test = None, n_periods = 6):
     return res
 
 # Define function
-def fit_sarimax(zc, endog_train, endog_test, order, seasonal_order = (0,0,0,12),exog_train = None, exog_test = None):
+def refit_sarimax(fit, zc, endog_train, endog_test,exog_train = None, exog_test = None):
     '''
     Input: 
+        fit : a previously fitted auto-arima model with initial starting params
         zc : target zipcode
         endog_train : training portion of the target time series variable
-        exog_train : (Optional) training portion of the feature covariate time series
         endog_test : testing portion of target time series
+
+        exog_train : (Optional) training portion of the feature covariate time series
         exog_test : (Optional) testing portino of the feature covariate time series
         
     Return:
         Best Parameters for SARIMAX Model along with cross validation scores
     '''
-
-    # Run a grid search with pdq and seasonal pdq parameters and get the best BIC value
     ans = []
     print(zc)
-    #pbar = tqdm(total=len(pdq) * len(pdqs))  # Initialize the progress bar
 
     
-    # SARIMAX Modelt 
-    sxmodel = ARIMA(order, seasonal_order, start_params=None, method='lbfgs', with_intercept = True, trend = 'c')
-    sx_fit = sxmodel.fit(y = endog_train, X = exog_train)
-    mape, mae, mse = mod_eval(sx_fit, endog_test, exog_test)
-    comb = sxmodel.order
-    combs = sxmodel.seasonal_order
-    ans.append([zc, comb, combs, sxmodel.aic(), sxmodel.bic(),mape, mae, mse])
+    # SARIMAX Model Refit
+    newfit = fit.update(y = endog_train, X = exog_train, maxiter = 100)
+    mape, mae, mse = mod_eval(newfit, endog_test, exog_test)
+    comb = newfit.order
+    combs = newfit.seasonal_order
+    ans.append([zc, comb, combs, newfit.aic(), newfit.bic(),mape, mae, mse])
     
     # Convert into dataframe
     ans_df = pd.DataFrame(ans, columns=['zipcode', 'pdq', 'pdqs', 'aic', 'bic','MAPE', 'MAE', 'MSE'])
     
-    return ans_df
+    return [ans_df, newfit]
 
-def find_pdqs(y, X=None):
+def arima_findpdqs(y, X=None):
     # Pre-compute d and D
     d = ndiffs(y, alpha = 0.05)
     D = nsdiffs(y, m = 12)
@@ -86,40 +84,47 @@ def find_pdqs(y, X=None):
                             start_P=0, start_Q=0, max_P=2, max_Q=2, D = D,
                             seasonal=True, trace=True, error_action='ignore',
                             suppress_warnings=True, stepwise=True, information_criterion='bic')
-    return([sxmodel.order, sxmodel.seasonal_order])
+    return([sxmodel.order, sxmodel.seasonal_order, sxmodel])
 
-def allzips_cv(zcs, df_trainval, exog_var_names = None):
-    df_list = []
-    for zc in zcs:
-        # Subset one zipcode
-        data = df_trainval[df_trainval['census_zcta5_geoid'] == zc].set_index('date')
-        data = data.asfreq('MS')
-        y = data['sfr_rental_index']
+def autoarima_cv(zc, df, exog_var_names = None, type = 'Univariate', traintest_cutoff = ['2022-12-01','2023-01-01']):
+    # Subset zipcode zc
+    data = df[df['census_zcta5_geoid'] == zc].set_index('date')
+    data = data.asfreq('MS')
+    data_trainval = data.loc[:traintest_cutoff[0]]
+    y_trainval = data.loc[:traintest_cutoff[0]]['sfr_rental_index']
+    y_holdout = data.loc[traintest_cutoff[1]:]['sfr_rental_index']
+    if exog_var_names:
+        X_trainval, X_holdout = data.loc[:traintest_cutoff[0]][exog_var_names], data.loc[traintest_cutoff[1]:][exog_var_names]
+    else:
+        X_trainval, X_holdout = None, None
+    # Get suggested order params from pm auto arima on training data and initial model
+    order, seasonal_order, fitfull = arima_findpdqs(y = y_trainval, X=X_trainval)
+    # Train, Test, Split
+    tscv = TimeSeriesSplit(n_splits = 3, max_train_size=None, test_size = 6) # hold out 6 months as test set
+    res_sari_L = [] # instantiate empty list for storing
+    
+    for i, (train_index, test_index) in enumerate(tscv.split(data_trainval)):
+        y_train, y_test = y_trainval.iloc[train_index], y_trainval.iloc[test_index]
+
         if exog_var_names:
-            X = data[exog_var_names]
-        # Get suggested order params from pm auto arima
-        order, seasonal_order = find_pdqs(y = y)
-        # Train, Test, Split
-        tscv = TimeSeriesSplit(n_splits = 10, max_train_size=None, test_size = 6) # hold out 6 months as test set
-        res_sari_L = []
-        for i, (train_index, test_index) in enumerate(tscv.split(data)):
-            print('TRAIN:', train_index, 'TEST:', test_index) 
-            if exog_var_names:
-                X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-            else:
-                X_train = None
-                X_test = None
-            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-            # Fit
-            res_sari = fit_sarimax(zc, endog_train = y_train, endog_test = y_test, 
-                                   order = order, seasonal_order = seasonal_order,
+            X_train, X_test = X_trainval.iloc[train_index], X_trainval.iloc[test_index]
+        else:
+            X_train, X_test = None, None
+            # Update the fit - Update the model fit with additional observed endog/exog values. Updating an ARIMA adds new observations to the model, updating the MLE of the parameters accordingly by performing several new iterations
+        [res_sari, fit] = refit_sarimax(fitfull, zc, endog_train = y_train, endog_test = y_test, 
                                   exog_train = X_train, exog_test = X_test)
-            res_sari['crossfold'] = i
-            res_sari['type'] = 'Univariate SARIMA'
-            res_sari_L.append(res_sari)
-        res_cv = pd.concat(res_sari_L)
-    df_list.append(res_cv) 
-    return(df_list)
+        res_sari['crossfold'] = i
+        res_sari['type'] = type
+        res_sari_L.append(res_sari)
+    # Get Final Holdout Test Score
+    cols = pd.concat(res_sari_L).columns
+    mape, mae, mse = mod_eval(fitfull, y_holdout, X_holdout, n_periods = 6)
+    test_res = pd.DataFrame({cols[0]:zc, cols[1]:[fitfull.order], cols[2]:[fitfull.seasonal_order], 
+                                 cols[3]:fitfull.aic(), cols[4]:fitfull.bic(),cols[5]:mape, cols[6]:mae, 
+                                 cols[7]:mse, cols[8]:'test score', cols[9]: 'Holdout Test ' + type})
+    res_sari_L.append(test_res)
+    res_cv = pd.concat(res_sari_L)
+    return(res_cv)
 
 
 # Load data, sort on zip and date and set index to datetime
@@ -140,20 +145,34 @@ missing_data = df[df['mfr_mean_rent_index'].isnull()]
 zcs_to_rm = missing_data['census_zcta5_geoid'].unique().tolist()
 
 # Filter rows where the 'zipcode' column is NOT in nan_zipcodes
-df_filtered = df.loc[~df['census_zcta5_geoid'].isin(zcs_to_rm)]
-df_trainval = df_filtered[df_filtered['date']<= '2022-12-01']
+df = df.loc[~df['census_zcta5_geoid'].isin(zcs_to_rm)]
 zcs = df_filtered.census_zcta5_geoid.unique()
 
 ################################ CROSS VALIDATION / FIT MODEL Univariate ##############################################
+uni_ = []
+for zc in zcs:
+    univar_res = autoarima_cv(zc, df, exog_var_names = None)
+    uni_.append(univar_res)
+univar_df = pd.concat(uni_)
+univar_df['mmape'] = univar_df.groupby(['zipcode', 'type'])['MAPE'].transform('mean')
+plot_df = univar_df[['zipcode', 'type', 'mmape']].drop_duplicates()
+univar_df.to_pickle('../data/sarimax_univariate_cv_results.pkl')
 
-univar_res = allzips_cv(zcs, df_trainval)
+sns.displot(plot_df, x="mmape", hue="type", kind="kde")
 
-univar_res.to_pickle('../data/sarimax_cv_results.pkl')
+####################################### MULTIVARIATE  ###################################################
+X_var_names = ['sfr_price_index', 'mfr_mean_rent', 'mfr_occ', 'cos_month', 'sin_month']
+multi_ = []
+for zc in zcs:
+    multi_res = autoarima_cv(zc, df, exog_var_names=X_var_names, type = 'Multivariate')
+    multi_.append(multi_res)
+
+multi_df = pd.concat(multi_)
+multi_df['mmape'] = multi_df.groupby(['zipcode', 'type'])['MAPE'].transform('mean')
+multi_df.to_pickle('../data/sarimax_multivariate_cv_results.pkl')
+# PLOT
+plot_df = pd.concat([univar_df, multi_df])[['zipcode', 'type', 'mmape']].drop_duplicates()
+
+sns.displot(plot_df, x="mmape", hue="type", kind="kde")
 
 
-####################################### MULTIVARIATE ###################################################
-exog_var_names = ['sfr_price_index', 'mfr_mean_rent_index', 'mfr_mean_occ_index', 'sin_month', 'cos_month']
-
-
-
-###################################### HOLDOUT TEST
